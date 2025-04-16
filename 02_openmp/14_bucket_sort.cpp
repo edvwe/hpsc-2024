7cdd8c78098da108ccc9c0d33696d76108eebeba
Edvard <wetind.e.fe33@m.isct.ac.jp>
2025-04-16 12:37:15 +0900

    finished first homework

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -2,9 +2,11 @@
 #include <cstdlib>
 #include <vector>
 #include <omp.h>
+#include <chrono>
 
 int main()
 {
+
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
@@ -22,21 +24,27 @@ int main()
   std::vector<int> offset(range, 0);
   for (int i = 1; i < range; i++)
     offset[i] = offset[i - 1] + bucket[i - 1];
+  auto tic = std::chrono::steady_clock::now();
 
-#pragma omp parallel for
   for (int i = 0; i < range; i++)
   {
     const int start_pos = offset[i];
     const int end_pos = start_pos + bucket[i];
+
+#pragma omp parallel for
     for (int j = start_pos; j < end_pos; j++)
     {
       key[j] = i;
     }
   }
+  auto toc = std::chrono::steady_clock::now();
 
   for (int i = 0; i < n; i++)
   {
     printf("%d ", key[i]);
   }
   printf("\n");
+
+  double time = std::chrono::duration<double>(toc - tic).count();
+  printf("time = %lf\n", time);
 }
